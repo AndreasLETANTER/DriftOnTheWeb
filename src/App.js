a963fa178b447c588a4c206e852e83bdd78625b5
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-07-05 16:39:38 +0200

    [MODIFY]: change setup of the scene for future test

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import { Car } from './Car';
 function CarShow() {
   return (
     <>
-      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} autoRotate autoRotateSpeed={0.4}/>
-      <PerspectiveCamera makeDefault position={[3, 2.3, 5]} fov={50} />
-      <color attach="background" args={['#000002']} />
-
+      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45}/>
+      <PerspectiveCamera makeDefault position={[10, 20, -30]} fov={50}/>
+      <color attach="background" args={['#000002']}/>
+    
       <spotLight
         color={[0.14, 0.5, 1]}
         intensity={2}
@@ -21,43 +21,7 @@ function CarShow() {
         castShadow
         shadow-bias={-0.0001}
       />
-      <spotLight
-        color={[0.14, 0.5, 1]}
-        intensity={2}
-        position={[-5, 5, 0]}
-        angle={0.6}
-        penumbra={0.5}
-        castShadow
-        shadow-bias={-0.0001}
-      />
-      <spotLight
-        color={[0.14, 0.5, 1]}
-        intensity={2}
-        position={[0, 5, 5]}
-        angle={0.6}
-        penumbra={0.5}
-        castShadow
-        shadow-bias={-0.0001}
-      />
-      <spotLight
-        color={[0.14, 0.5, 1]}
-        intensity={2}
-        position={[0, 5, -5]}
-        angle={0.6}
-        penumbra={0.5}
-        castShadow
-        shadow-bias={-0.0001}
-      />
-      <spotLight
-        color={[84, 46, 113]}
-        intensity={0.2}
-        position={[0, 10, 2]}
-        angle={0.3}
-        penumbra={0.5}
-        castShadow
-        shadow-bias={-0.0001}
-      />
-
+      <pointLight position={[0, 10, 0]} intensity={3} />
       <Ground/>
       <Car/>
     </>
