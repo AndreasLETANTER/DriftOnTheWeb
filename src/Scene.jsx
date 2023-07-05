d3671aa2444c71c00dd549b03591ea4fd78934f3
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-07-05 18:11:35 +0200

    [MODIFY]: restarted the project to zero due to difficulties moving the car, added basic environment

---
+++ src/Scene.jsx
@@ -0,0 +1,15 @@
+import { Environment, OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { Suspense } from 'react';
+
+export function Scene() {
+    return (
+        <Suspense fallback={null}>
+            <Environment
+                files={process.env.PUBLIC_URL + '/textures/envmap.hdr'}
+                background={"both"}
+            />
+            <PerspectiveCamera makeDefault position={[-6, 3.9, 6.21]} fov={50} />
+            <OrbitControls target={[-2.64, -0.71, 0.03]}/>
+        </Suspense>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import App from './App';
-import { createRoot } from 'react-dom/client';
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import { Scene } from "./Scene";
 
-const root = document.getElementById('root');
-createRoot(root).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <Canvas>
+    <Scene />
+  </Canvas>
+);
