da968179dfa4855545a5a114da74794a6b6db9d8
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-07-06 17:04:49 +0200

    [ADD]: physics component with SAP algorithm collision detection and low gravity

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import "./index.css";
 import { createRoot } from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import { Scene } from "./Scene";
+import { Physics } from "@react-three/cannon";
 
 createRoot(document.getElementById("root")).render(
   <Canvas>
-    <Scene />
+    <Physics gravity={[0, -30, 0]} broadphase="SAP">
+      <Scene />
+    </Physics>
   </Canvas>
 );
