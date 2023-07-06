3eed88608a632d460698e066994f954e361cc0d6
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-07-06 17:41:59 +0200

    [ADD]: wheels display debug with physics but not grouping whell with the car chassis

--- src/Car.jsx
+++ src/Car.jsx
@@ -1,7 +1,9 @@
-import { useBox } from '@react-three/cannon';
+import { useBox, useRaycastVehicle } from '@react-three/cannon';
 import { useFrame, useLoader } from '@react-three/fiber';
 import { useEffect, useRef } from 'react';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { useWheels } from './useWheels';
+import { WheelDebug } from './WheelDebug';
 
 export function Car() {
     let mesh = useLoader(GLTFLoader, process.env.PUBLIC_URL + '/models/car.glb').scene;
@@ -21,15 +23,31 @@ export function Car() {
         useRef(null),
     );
 
+    const [wheels, wheelInfos] = useWheels(width, height, front, wheelRadius);
+    const [vehicle, vehicleApi] = useRaycastVehicle(
+        () => ({
+            chassisBody,
+            wheels,
+            wheelInfos,
+        }),
+        useRef(null),
+    );
+
     useEffect(() => {
         mesh.scale.set(0.0012, 0.0012, 0.0012);
         mesh.children[0].position.set(-365, -18, -67);
     }, [mesh]);
     return (
         //<primitive object={mesh} rotation-y={Math.PI}/>
-        <mesh ref={chassisBody} castShadow>
-            <meshBasicMaterial color={'red'}/>
-            <boxGeometry args={chassisBodyArgs}/>
-        </mesh>
+        <group ref={vehicle} name='vehicle'>
+            <mesh ref={chassisBody} castShadow>
+                <meshBasicMaterial color={'red'}/>
+                <boxGeometry args={chassisBodyArgs}/>
+            </mesh>
+            <WheelDebug radius={wheelRadius} wheelRef={wheels[0]}/>
+            <WheelDebug radius={wheelRadius} wheelRef={wheels[1]}/>
+            <WheelDebug radius={wheelRadius} wheelRef={wheels[2]}/>
+            <WheelDebug radius={wheelRadius} wheelRef={wheels[3]}/>
+        </group>
     );
 }
