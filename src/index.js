83d12014f1e5fb8b0da963cbc5df5d09f1f41259
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-07-13 16:01:50 +0200

    [ADD]: start button for launching the game

--- src/index.js
+++ src/index.js
@@ -3,11 +3,36 @@ import { createRoot } from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import { Scene } from "./Scene";
 import { Physics } from "@react-three/cannon";
+import { useState } from "react";
+
+function Start() {
+  return (
+    <Canvas>
+      <Physics gravity={[0, -4.8, 0]} broadphase="SAP">
+        <Scene/>
+      </Physics>
+    </Canvas>
+  );
+}
+
+function Help({ noHelpNeeded }) {
+  return (
+    <div className="help">
+      <button onClick={noHelpNeeded}>Start</button>
+    </div>
+  );
+}
+
+function Intro() {
+  const [helpNeeded, setHelpNeeded] = useState(true);
+
+  const noHelpNeeded = () => {
+    setHelpNeeded(false);
+  };
+  console.log(helpNeeded);
+  return helpNeeded ? <Help noHelpNeeded={noHelpNeeded}/> : <Start/>
+}
 
 createRoot(document.getElementById("root")).render(
-  <Canvas>
-    <Physics gravity={[0, -4.8, 0]} broadphase="SAP">
-      <Scene/>
-    </Physics>
-  </Canvas>
+  <Intro/>
 );
