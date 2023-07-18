b4a57c777018dc98d02c54e0416e1c688bbc1a7e
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-07-18 17:07:45 +0200

    [FIX]: disable debug mode for trackbox

--- src/TrackBox.jsx
+++ src/TrackBox.jsx
@@ -1,7 +1,7 @@
 import { useBox } from "@react-three/cannon";
 import { setCarOnTrack, setCarNotOnTrack } from "./Car";
 
-const debug = true;
+const debug = false;
 
 export function TrackBox({ position, scale }) {
 
